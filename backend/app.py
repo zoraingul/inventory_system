c273e028790d53376cc915e1972e39b339e4015d
zoraingul <zorain.stu@gmail.com>
2025-12-07 01:33:34 +0500

    Add categories modules with CRUD operations

--- backend/app.py
+++ backend/app.py
@@ -75,6 +75,64 @@ def login():
         return jsonify({'error': 'Inalid username or password'}) , 401
         
 
+@app.route('/categories' , methods = ['POST'])
+def create_category():
+    data = request.get_json()
+    category_name = data.get('category_name')
+    description = data.get('description')
+    created_by = data.get('created_by')
+    
+    #validation
+    if not category_name or not created_by:
+        return jsonify({'error' : 'Category name and creator are required'}) , 400
+    
+    try:
+        cursor.execute(
+            "insert into categories (category_name , description , created_by) VALUES (%s , %s , %s)" , (category_name , description , created_by)
+    )
+        db.commit()
+        return jsonify({'message': 'Category created successfully'})
+    
+    except mysql.connector.Error as e:
+        return jsonify({'error' , str(e)}) , 500
+    
+@app.route('/catetgories' , methods = ["GET"])
+def get_categories():
+    cursor.execute("Select * from categories")
+    categories = cursor.fetchall()
+    return jsonify(categories)
+    
+    
+@app.route('/categories/<int:category_id>' , methods = ["PUT"])
+def update_category(category_id):
+    
+    data = request.get_json()
+    category_name = data.get('category_name')
+    description = data.get('description')
+    
+    if not category_name or not description: #created by confusion
+        return jsonify({'error' : 'Nothing to update'})
+
+    try:
+        cursor.execute(
+            "Update categories SET category_name= %s , description=%s where category_id=%s" , (category_name ,description , category_id)
+        )
+        db.commit()
+        return jsonify({'message' : "Categry updated successfully"})
+    except mysql.connector.Error as e:
+        return jsonify({'error' : str(e)}) , 500
+
+@app.route('/categories/<int:category_id>' , methods = ['DELETE'])
+def delete_category(category_id):
+    try:
+        cursor.execute("DELETE from category where category_id=%s" , (category_id))
+        db.commit()
+        return jsonify({'message' : 'Category Deleted Successfully'})
+    except mysql.connector.Error as e:
+        return jsonify ({'error' : str(e)}) , 500
+
+
+
 @app.route('/')
 def home():
     return "Backend is running successfully"
